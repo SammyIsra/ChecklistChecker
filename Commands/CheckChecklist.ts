d0a8d420dc5044836e73c36ff4b6792d7c7eaa3a
Sammy Israwi <sammy.israwi@gmail.com>
2018-03-02 20:55:15 -0500

    Created initial commands and cli tool settings. Functionality still does not exist :(

---
+++ Commands/CheckChecklist.ts
@@ -0,0 +1,3 @@
+function CreateChecklist(): void {
+  
+}
---
+++ Index.ts
@@ -0,0 +1,25 @@
+const program = require("commander");
+// const HandleCreate = require("./Commands/CreateChecklist");
+// const HandleCheck = require("./Commands/CheckChecklist");
+
+
+program
+  .version("0.0.1")
+  .name("checklist")
+  .description("Checklist Checker")
+
+program
+  .command("check [fileName]", {isDefault: true})
+  .alias("c")
+  .description("Check an existing checklist (Uses checklist.json by default)")
+  .action( (fileName: string):void => console.log(`You are checking a checklist! ${fileName}`));
+
+program
+  .command("create")
+  .alias("i")
+  .description("Initialize a checklist")
+  .action( ():void => console.log("created! (lol)"));
+
+//console.log(program);
+
+program.parse(process.argv);
